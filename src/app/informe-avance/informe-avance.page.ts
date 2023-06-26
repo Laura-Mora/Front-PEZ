bc7beac025cd944225f059084bd0967c1b1500bd
Laura-Mora <46944834+Laura-Mora@users.noreply.github.com>
2023-06-26 17:15:28 -0500

    Despliegue básico asignaturas cursadas

--- src/app/informe-avance/informe-avance.page.ts
+++ src/app/informe-avance/informe-avance.page.ts
@@ -1,5 +1,12 @@
 import { Component, OnInit } from '@angular/core';
 import { AlertController, ModalController, ToastController } from '@ionic/angular';
+import { Usuario } from '../Model/Usuario/usuario';
+import { Asignatura } from '../Model/Asignatura/asignatura';
+import { Programa } from '../Model/Programa/programa';
+import { LoginService } from '../servicios/login.service';
+import { ProgramaService } from '../Model/Programa/programa.service';
+import { PerfilEstudiante } from '../Model/Perfil-Estudiante/perfil-estudiante';
+import { PerfilEstudianteService } from '../Model/Perfil-Estudiante/perfil-estudiante.service';
 
 @Component({
   selector: 'app-informe-avance',
@@ -8,22 +15,51 @@ import { AlertController, ModalController, ToastController } from '@ionic/angula
 })
 export class InformeAvancePage implements OnInit {
 
-  public programas = ["Maestría en ingeniería de sistemas y computación", "Maestría en Analitica para la inteligencia de negocios"]
-  public asignaturas = ["a","b","c"]
-  public programasSuge = ["Maestría en seguridad digital"]
+  programas: Programa[]  = [];
+  asignaturas: Asignatura[] = [];
+  programasSuge: Programa[] = [];
+  usuario: Usuario | null;
+  perfil: PerfilEstudiante | undefined;
 
   constructor(
+    private loginService: LoginService,
     private modalController:ModalController, 
     public toastCtrl: ToastController,
-    public alertController: AlertController
-  ) { }
+    public alertController: AlertController,
+    private proService: ProgramaService,
+    private perfilService: PerfilEstudianteService
+  ) { 
+    this.usuario = this.loginService.getUser();
+    console.log(this.usuario);
+  }
 
   ngOnInit() {
+    this.findProgramas();
+    this.findAsignaturas();
   }
 
   closeModal(){
     this.modalController.dismiss();
   }
 
+  findProgramas(){
+    this.programas = this.usuario?.programa || [];
+  }
 
+  findAsignaturas(){
+    const idPer = this.usuario?.perfilEstudiante_id;
+    if (idPer !== undefined) {
+      this.perfilService.getPerfilById(idPer).subscribe(
+        results => {
+          this.perfil = results;
+          this.asignaturas = this.perfil.asignaturasCursadas;
+        },
+        error => {
+          console.error(error);
+        }
+      );
+
+    }
+
+  }
 }
