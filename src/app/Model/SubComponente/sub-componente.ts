d723da239aab40bf1a87c4aa3bb53e5d14b2f19d
Laura-Mora <46944834+Laura-Mora@users.noreply.github.com>
2023-08-21 17:42:10 -0500

    Mostrar más datos

--- src/app/Model/SubComponente/sub-componente.ts
+++ src/app/Model/SubComponente/sub-componente.ts
@@ -4,6 +4,7 @@ export class SubComponente {
     id!: number;
     nombre!: string;
     cantCreditos!: number;
+    creditosVistos!: number;
     cantAsiganturas!: number;
     asignaturas: Asignatura[] = []; //Asignaturas para mostrar el avance del programa
     asignaturasElectivas: Asignatura[] = [];
