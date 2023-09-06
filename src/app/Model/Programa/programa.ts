93f1f46f62a58a08a0d9d17cd2f28319ca1fb7d5
Laura-Mora <46944834+Laura-Mora@users.noreply.github.com>
2023-09-06 12:13:10 -0500

    Agregar datos avance programa

--- src/app/Model/Programa/programa.ts
+++ src/app/Model/Programa/programa.ts
@@ -5,6 +5,10 @@ export class Programa {
     programa!: string; //nombre del programa para avance
     nombre!: string;
     componentes: Componente[] = [];
-    
+    cantCreditos!: number;
+    cantCreditosCursados!: number;
+    requisitoIngles!: Boolean;
+    requisitoInglesCumplido!: Boolean;
+
     constructor(){}
 }
